d298c02d00f415c324206b4bdcf91107b71bd8f2
GNUSheep <pentet221@gmail.com>
2023-07-30 12:39:10 +0200

    Add rendering text by using bitmap

--- src/render.rs
+++ src/render.rs
@@ -4,13 +4,120 @@ use image::EncodableLayout;
 use std;
 use std::ffi::{CString, CStr, c_void};
 use std::path::Path;
+use std::collections::HashMap;
+use crate::scenes::game;
+
+pub struct Texts {
+    objects: Vec<Object>,
+    texture: Texture,
+    program: Program,
+    chars: Vec<char>,
+    positions: Vec<[f32; 8]>,
+}
+
+impl Texts {
+    pub fn init() -> Self {
+        let chars: Vec<char> = vec!['0','1','2','3','4','5','6','7','8','9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        let mut positions = vec![[0.0; 8]; 36];
+        
+        let mut y_bitmap = 0;
+        let mut i = 0;
+        for mut j in 0..=35 {
+            if i == 16 {
+                y_bitmap += 1;
+                i = 0;
+            }
+
+            positions[i+y_bitmap*16][0] = (8.0+9.0*i as f32)/143.0;
+            positions[i+y_bitmap*16][1] = (0.0+9.0*y_bitmap as f32)/26.0;
+            positions[i+y_bitmap*16][2] = (8.0+9.0*i as f32)/143.0;
+            positions[i+y_bitmap*16][3] = (8.0+9.0*y_bitmap as f32)/26.0;
+            positions[i+y_bitmap*16][4] = (0.0+9.0*i as f32)/143.0;
+            positions[i+y_bitmap*16][5] = (8.0+9.0*y_bitmap as f32)/26.0;
+            positions[i+y_bitmap*16][6] = (0.0+9.0*i as f32)/143.0;
+            positions[i+y_bitmap*16][7] = (0.0+9.0*y_bitmap as f32)/26.0;
+
+            i += 1;
+        }
+
+        let bitmap: HashMap<_, _> = chars.iter().zip(positions.iter()).collect();
+        let texture = Texture::create_new_texture_from_file(&Path::new("src/scenes/game/assets/images/font_bitmap.png"));
+        let mut objects: Vec<Object> = vec![];
+        
+        let vert_shader = Shader::vertex_from_src(
+            &CString::new(include_str!("scenes/game/assets/shaders/text.vert")).unwrap(),
+        ).unwrap();
+
+        let frag_shader = Shader::fragment_from_src(
+            &CString::new(include_str!("scenes/game/assets/shaders/text.frag")).unwrap(),
+        ).unwrap();
+        
+        let program = Program::create_with_shaders(&[vert_shader, frag_shader]).unwrap();
+
+        Self{objects, texture, program, chars, positions}
+    }
+
+    pub fn add_text(&mut self, text: String, x: f32, y: f32) {
+        let bitmap: HashMap<_, _> = self.chars.iter().zip(self.positions.iter()).collect();
+
+        let text_vec_char: Vec<_> = text.to_lowercase().chars().collect();
+        const INDCIES: [i32; 6] = [
+            0, 1, 2,
+            2, 3, 0
+        ];
+    
+        let mut index = 0;
+        for c in text_vec_char.iter() {
+            let pos = bitmap.get(c).unwrap();
+            let points: Vec<f32> = vec![
+                (x+(8.0/256.0)*index as f32)+8.0/256.0, y+8.0/240.0, 0.0, pos[0], pos[1],
+                (x+(8.0/256.0)*index as f32)+8.0/256.0, y-8.0/240.0, 0.0, pos[2], pos[3],
+                (x+(8.0/256.0)*index as f32)-8.0/256.0, y-8.0/240.0, 0.0, pos[4], pos[5],
+                (x+(8.0/256.0)*index as f32)-8.0/256.0, y+8.0/240.0, 0.0, pos[6], pos[7]
+            ];
+
+            index += 2;
+            let obj = Object::create_square_with_points(points, INDCIES);
+            self.objects.push(obj);
+        }
+
+        unsafe {
+            for obj in self.objects.iter() {
+                obj.set_vertex_attrib_pointer(0, 
+                    3, 
+                    gl::FLOAT, 
+                    gl::FALSE, 
+                    (5 * std::mem::size_of::<f32>()) as gl::types::GLint, 
+                    std::ptr::null()
+                );
+                
+                obj.set_vertex_attrib_pointer(1, 
+                    2, 
+                    gl::FLOAT, 
+                    gl::FALSE, 
+                    (5 * std::mem::size_of::<f32>()) as gl::types::GLint, 
+                    (3 * std::mem::size_of::<f32>()) as *const c_void, 
+                );
+            }
+        }
+    }
+
+    pub unsafe fn draw(&self) {
+        self.program.set_active();
+        gl::BindTexture(gl::TEXTURE_2D, self.texture.texture);
+        for obj in self.objects.iter() {
+            gl::BindVertexArray(obj.vao);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
+        }
+    }
+}
 
 pub struct Texture {
     pub texture: gl::types::GLuint,
 }
 
 impl Texture {
-    pub fn create_new_texture_from_file(file_path: &Path) -> Texture {
+    pub fn create_new_texture_from_file(file_path: &Path) -> Self {
         let mut texture: gl::types::GLuint = 0;
         unsafe {
             gl::GenTextures(1, &mut texture);
@@ -42,7 +149,7 @@ impl Texture {
             gl::Enable(gl::BLEND);
         }
         
-        return Texture{texture}
+        Self{texture}
     }
 }
 
